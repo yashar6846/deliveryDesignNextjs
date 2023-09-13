c17f114446390ad777b94c335456d9e79d686e86
Yashar Najafi <yasharnajafi6946@gmail.com>
2023-09-13 16:04:06 +0300

    add design slider css

--- app/components/Featured.tsx
+++ app/components/Featured.tsx
@@ -1,8 +1,33 @@
+import Image from 'next/image'
 import React from 'react'
+import { featuredProducts } from '../data'
 
 function Featured() {
   return (
-    <div>Featured</div>
+    <div className='w-screen overflow-scroll text-red-500'>
+        {/* Wrapper */}
+        <div className='w-max flex'>
+            {/* Single Item */}
+
+            {featuredProducts.map((item) => (
+            <div className='w-screen h-[60vh] flex flex-col items-center justify-around p-4 hover:bg-fuchsia-50 transition-all duration-300 md:w-[50vw] xl:w-[33vw] xl:h-[9ovh]'>
+                {/* Image Container  */}
+                {item.img && (
+                <div key={item.id} className='relative flex-1 w-full hover:rotate-[60deg] transition-all duration-500'>
+                    <Image src={item.img} alt='' fill className=' object-contain' />
+                </div>
+                )}
+                {/* Text Coutainer */}
+                <div className='flex-1 flex flex-col items-center justify-center text-center gap-4'>
+                    <h1 className='text-xl font-bold uppercase xl:text-2xl 2xl:text-3xl '>{item.title}</h1>
+                    <p className='p-2'>{item.desc}</p>
+                    <span className='text-xl font-bold'>${item.price}</span>
+                    <button className='bg-red-500 text-white p-2 rounded-md'>Add to Cart</button>
+                </div>
+            </div>
+             ))}
+        </div>
+    </div>
   )
 }
 
